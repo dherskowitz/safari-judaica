1c8ecf0a781fdd7da4db121583a017794e09bf9d
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2020-07-26 17:19:59 +0300

    add custom colors for stock badge

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,16 @@ module.exports = {
                     default: "#dcd7ce",
                     lighter: "#b2aea975",
                     darker: "#b2aea9",
+                    alt: "#bda27f",
                 },
                 outerSpace: {
                     default: "#2E3131",
                     lighter: "#434848",
+                },
+                stock: {
+                    default: "#1e824c",
+                    low: "#6c7a89",
+                    out: "#d91e18",
                 },
             },
         },
