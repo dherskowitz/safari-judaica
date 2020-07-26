d740b4e613f6d76acc743f86aa2f4ce76ca8d77c
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2020-07-26 16:29:33 +0300

    add shop page

---
+++ src/pages/Shop.vue
@@ -0,0 +1,29 @@
+<template>
+    <Layout>
+        <section class="min-h-screen md:flex md:items-start md:max-w-full">
+            <div class="mt-10 lg:mt-20 flex-none w-full md:max-w-xs">
+                <CategoriesMenu />
+            </div>
+            <div class="flex-1 w-full">
+                <NewProducts />
+                <PopularProducts />
+            </div>
+        </section>
+    </Layout>
+</template>
+
+<script>
+import CategoriesMenu from "~/components/CategoriesMenu";
+import PopularProducts from "~/components/PopularProducts";
+import NewProducts from "~/components/NewProducts";
+export default {
+    components: {
+        CategoriesMenu,
+        PopularProducts,
+        NewProducts,
+    },
+};
+</script>
+
+<style>
+</style>
