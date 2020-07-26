bc1abb1163da851e3f1933c931652bc773729277
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2020-07-26 16:28:36 +0300

    add categories menu component

---
+++ src/components/NewProducts.vue
@@ -0,0 +1,48 @@
+<template>
+    <section class="mt-10 lg:mt-20">
+        <h1 class="text-3xl font-semibold border-gray-600 border-b-4 mb-8">New Products</h1>
+        <div class="grid grid-flow-row sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-2">
+            <ProductCard
+                v-for="edge in $static.products.edges"
+                :key="edge.node.id"
+                :productData="edge"
+            />
+        </div>
+    </section>
+</template>
+
+<script>
+import ProductCard from "~/components/ProductCard";
+export default {
+    name: "NewProducts",
+    components: {
+        ProductCard,
+    },
+};
+</script>
+
+<static-query>
+query {
+  products: allProduct(filter: {newProduct: {eq: true}}) {
+    edges {
+      node {
+        id
+        name
+        priceDollars
+        summary
+        stockStatus
+        primaryImage {
+          thumbnails {
+            large {
+              url
+            }
+          }
+        }
+      }
+    }
+  }
+}
+</static-query>
+
+<style>
+</style>
