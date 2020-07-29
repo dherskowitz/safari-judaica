2816d2a734c728844479b0dfec62e5becc141a2e
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2020-07-29 13:27:01 +0300

    continue w/ page styles, add similar products

--- src/templates/Product.vue
+++ src/templates/Product.vue
@@ -1,8 +1,17 @@
 <template>
     <Layout>
-        {{$page.product.id}}
-        <div class="bg-white rounded-lg shadow-lg mt-10 md:mt-20 flex">
-            <div class="w-1/2 mr-10 flex flex-col justify-between border-capeHoney-alt border-r-4">
+        <div class="mt-10">
+            <g-link to="/" class="mr-1 hover:underline">Home</g-link>/
+            <g-link
+                :to="categoryLink"
+                class="mr-1 hover:underline"
+            >{{ $page.product.categoryName[0] }}</g-link>/
+            <span class="text-capeHoney-alt">{{ $page.product.name }}</span>
+        </div>
+        <div class="mt-10 md:bg-white flex flex-col md:flex-row">
+            <div
+                class="bg-white rounded-lg md:rounded-none shadow-lg md:shadow-none w-full md:w-1/2 md:mr-10 flex flex-col justify-between border-capeHoney-alt md:border-r-4"
+            >
                 <div class="mainProductImage">
                     <g-image :src="mainImage" :alt="$page.product.name"></g-image>
                 </div>
@@ -24,23 +33,35 @@
                     ></g-image>
                 </div>
             </div>
-            <div class="w-1/2 pt-4 pr-4 pb-4 leading-loose">
-                <h1 class="text-3xl">{{ $page.product.name }}</h1>
+            <div
+                class="bg-white rounded-lg md:rounded-none shadow-lg md:shadow-none w-full md:w-1/2 md:mr-10 text-center md:text-left mt-10 md:mt-0 pt-4 pb-4 leading-loose"
+            >
+                <h1 class="text-3xl mb-2 border-b-4 border-capeHoney-alt">{{ $page.product.name }}</h1>
                 <p
                     class="py-2 italic text-xl text-grey-600 font-semibold"
                 >${{ $page.product.priceDollars.toFixed(2) }}</p>
-                <p>
-                    Category:
-                    <a
-                        :href="'/shop/category/'+ category"
-                    >{{ $page.product.categoryName[0] }}</a>
-                </p>
                 <p>{{ $page.product.stockStatus }}</p>
                 <p>{{ $page.product.summary }}</p>
-                <p>Measures 10"w x 20"h</p>
-                <p>Weights 0.5 lbs</p>
-                <p>Material is Leather and Fur</p>
-                <div>
+                <p>
+                    <span class="text-lg font-bold text-gray-700">- Category</span>&nbsp;&nbsp;
+                    <span v-for="catName, i in $page.product.categoryName">
+                        <span v-if="i > 0">/&nbsp;&nbsp;</span>
+                        <a
+                            class="text-capeHoney-alt underline text-left"
+                            :href="'/shop/'+catName.replace(' ', '-').toLowerCase()"
+                        >{{ catName }}</a>&nbsp;&nbsp;
+                    </span>
+                </p>
+                <p>
+                    <span class="text-lg font-bold text-gray-700">- Dimensions</span>&nbsp;&nbsp; 10"w x 20"h
+                </p>
+                <p>
+                    <span class="text-lg font-bold text-gray-700">- Weight</span>&nbsp;&nbsp; 0.5 lbs
+                </p>
+                <p>
+                    <span class="text-lg font-bold text-gray-700">- Materials</span>&nbsp;&nbsp; Leather and Fur
+                </p>
+                <div class="mt-5">
                     Share:
                     <span class="mr-2">Facebook</span>
                     <span class="mr-2">Instagram</span>
@@ -48,14 +69,18 @@
                 </div>
 
                 <button
-                    class="bg-outerSpace p-2 mt-10 text-white rounded-lg shadow-lg"
+                    class="bg-outerSpace p-2 px-4 mt-10 text-white rounded-lg shadow-lg md:w-full uppercase text-base md:text-lg"
                     @click="toggleForm"
                 >Inquire about this product!</button>
             </div>
         </div>
-        <div class="bg-white rounded-lg shadow-lg mt-10 md:mt-20 p-10">
+        <div class="mt-10 md:mt-20">
             <h2 class="text-3xl mb-2 border-b-4 border-capeHoney-alt">Product Description</h2>
-            <p>{{ $page.product.description }}</p>
+            <p class="bg-white rounded-lg shadow-lg mt-10 p-10">{{ $page.product.description }}</p>
+        </div>
+        <div class="mt-10 md:mt-20">
+            <h2 class="text-3xl mb-8 border-b-4 border-capeHoney-alt">Similar Products</h2>
+            <ProductCard v-for="edge in relatedProducts" :key="edge.node.id" :productData="edge" />
         </div>
         <InquiryForm
             v-bind:showForm="showForm"
@@ -66,46 +91,67 @@
 </template>
 
 <page-query>
-query ($id: ID!) {
-  product(id: $id) {
-    id
-    name
-    sku
-    priceDollars
-    category
-    categoryName
-    stockStatus
-    newProduct
-    summary
-    description
-    primaryImage {
-      id
-      thumbnails {
-        large {
-          url
+query ($id: ID!, $Category__0: String!) {
+    product: product(id: $id) {
+        id
+        name
+        sku
+        path
+        priceDollars
+        category
+        categoryName
+        stockStatus
+        newProduct
+        summary
+        description
+        primaryImage {
+        id
+        thumbnails {
+            large {
+            url
+            }
+            full {
+            url
+            }
         }
-        full {
-          url
         }
-      }
+        additionalImages {
+        id
+        thumbnails {
+            large {
+            url
+            }
+            full {
+            url
+            }
+        }
+        }
     }
-    additionalImages {
-      id
-      thumbnails {
-        large {
-          url
+    relatedProducts: allProduct(filter: {category: {contains: [$Category__0]}}) {
+        edges {
+            node {
+                id
+                name
+                path
+                priceDollars
+                summary
+                stockStatus
+                primaryImage {
+                    thumbnails {
+                        large {
+                            url
+                        }
+                    }
+                }
+            }
         }
-        full {
-          url
-        }
-      }
     }
-  }
 }
 </page-query>
 
 <script>
 import InquiryForm from "~/components/InquiryForm";
+import ProductCard from "~/components/ProductCard";
 
 export default {
     data() {
@@ -113,7 +159,8 @@ export default {
             showForm: false,
             mainImage: "",
             mainImageID: "",
-            category: "",
+            categoryLink: "",
+            relatedProducts: [],
         };
     },
     metaInfo: {
@@ -122,15 +169,28 @@ export default {
     },
     components: {
         InquiryForm,
+        ProductCard,
+    },
+    beforeUpdate() {
+        this.relatedProducts = this.$page.relatedProducts.edges.filter(
+            (item) => {
+                return item.node.id !== this.$page.product.id;
+            }
+        );
     },
     mounted() {
+        console.log(this.$page);
         this.mainImage = this.$page.product.primaryImage[0].thumbnails.large.url;
         this.mainImageID = this.$page.product.primaryImage[0].id;
-        this.category = this.$page.product.categoryName[0]
+        let catSlug = this.$page.product.categoryName[0]
             .toLowerCase()
             .replace(" ", "-");
+        this.categoryLink = `/shop/${catSlug}`;
     },
     methods: {
+        // stringify(name) {
+        //     return name.replace(" ", "-").toLowerCase();
+        // },
         toggleForm() {
             this.showForm = !this.showForm;
             document
