d1df3a66c55d841ce417efcd1139d85f4349d842
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2020-07-31 13:58:58 +0300

    add breadcrumbs

--- src/templates/Category.vue
+++ src/templates/Category.vue
@@ -1,5 +1,9 @@
 <template>
     <Layout>
+        <div class="mt-10">
+            <g-link to="/shop" class="mr-1 hover:underline">Shop</g-link>/
+            <span class="text-capeHoney-alt">{{ titleCase($route.params.Name) }}</span>
+        </div>
         <section class="min-h-screen md:flex md:items-start md:max-w-full mt-10 lg:mt-20">
             <div class="flex-none w-full md:max-w-xs">
                 <CategoriesMenu />
@@ -55,6 +59,15 @@ export default {
     metaInfo: {
         title: "Shop By Category",
         meta: [{ name: "description", content: "Shop Safari Judaica" }],
+    },
+    methods: {
+        titleCase(str) {
+            let title = str.replace("-", " ").split(" ");
+            let titleCased = title.map((element) => {
+                return element.charAt(0).toUpperCase() + element.slice(1);
+            });
+            return titleCased.join(" ");
+        },
     },
 };
 </script>
