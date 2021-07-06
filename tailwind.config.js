184c8150ef778ef650cf9b02f4bdb984a024eb3c
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2021-07-06 11:08:45 +0300

    include maxWidth

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 module.exports = {
     purge: ["./src/**/*.html", "./src/**/*.vue", "./src/**/*.jsx"],
     theme: {
+        maxWidth: {
+            '1/4': '25%',
+            '1/2': '50%',
+            '3/4': '75%',
+        },
         extend: {
             colors: {
                 capeHoney: {
@@ -20,14 +25,6 @@ module.exports = {
                 },
             },
         },
-        // boxShadow: {
-        //     default:
-        //         "0 1px 3px 0 rgba(0, 0, 0, .1), 0 1px 2px 0 rgba(0, 0, 0, .06)",
-        //     md:
-        //         "0 4px 6px -1px rgba(0, 0, 0, .1), 0 2px 4px -1px rgba(0, 0, 0, .06)",
-        //     lg:
-        //         "0 10px 15px -3px rgba(0, 0, 0, .1), 0 4px 6px -2px rgba(0, 0, 0, .25)",
-        // },
     },
     variants: {
         boxShadow: ["responsive", "hover", "focus"],
