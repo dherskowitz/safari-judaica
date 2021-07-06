beed2bef2aec56057b875b178852938ae1792ca6
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2021-07-06 11:08:16 +0300

    formatting

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,51 +5,64 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-    siteName: "Safari Judaica",
-    plugins: [
-        {
-            use: "gridsome-plugin-tailwindcss",
-        },
-        {
-            use: "@gridsome/source-airtable",
-            options: {
-                apiKey: process.env.AIRTABLE_API_KEY, // required
-                base: process.env.AIRTABLE_BASE, // required
-                tables: [
-                    {
-                        name: "Products", // required
-                        typeName: "Product", // required
-                        select: {}, // optional
-                        links: [
-                            {
-                                fieldName: "Product Reviews",
-                                typeName: "Review",
-                            },
-                        ],
-                    },
-                    {
-                        name: "Product Reviews", // required
-                        typeName: "Review", // required
-                        select: {}, // optional
-                    },
-                    {
-                        name: "Categories", // required
-                        typeName: "Category", // required
-                        links: [
-                            // optional
-                            {
-                                fieldName: "Category",
-                                typeName: "Category",
-                                linkToFirst: true, // optional
-                            },
-                        ],
-                    },
-                ],
-            },
-        },
-    ],
-    templates: {
-        Product: "/shop/:CategoryName__0/:Slug",
-        Category: "/shop/:Name",
+  siteName: "Safari Judaica",
+  plugins: [
+    {
+      use: "gridsome-plugin-tailwindcss",
     },
+    {
+      use: "@gridsome/source-airtable",
+      options: {
+        apiKey: process.env.AIRTABLE_API_KEY, // required
+        base: process.env.AIRTABLE_BASE, // required
+        tables: [
+          {
+            name: "Products", // required
+            typeName: "Product", // required
+            select: {}, // optional
+            links: [
+              {
+                fieldName: "Product Reviews",
+                typeName: "Review",
+              },
+            ],
+          },
+          {
+            name: "Product Reviews", // required
+            typeName: "Review", // required
+            select: {}, // optional
+          },
+          {
+            name: "Categories", // required
+            typeName: "Category", // required
+            links: [
+              // optional
+              {
+                fieldName: "Category",
+                typeName: "Category",
+                linkToFirst: true, // optional
+              },
+            ],
+          },
+          {
+            name: "Collections", // required
+            typeName: "Collection", // required
+            links: [
+              // optional
+              {
+                fieldName: "Collection",
+                typeName: "Collection",
+                linkToFirst: true, // optional
+              },
+            ],
+          },
+        ],
+      },
+    },
+  ],
+  templates: {
+    Product: "/shop/:CategoryName__0/:Slug",
+    Category: "/shop/:Name",
+    Collection: "/shop/:Name",
+  },
 };
