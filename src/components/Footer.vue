a2301b07b6a5ffc5100ec823daf4f1c7c8160ed1
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2021-07-06 11:09:37 +0300

    move to component

---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+            <footer class="mt-20 bg-outerSpace-default">
+            <div class="p-10">
+                <g-image src="~/assets/img/Safari_Juciaca_Logo-1024x864.png" width="200" />
+            </div>
+            <div
+                class="py-3 text-center text-white bg-outerSpace-lighter"
+            >Copyright {{ currentYear }} All Rights Reserved SafariJudaica.com</div>
+        </footer>
+</template>
+
+<script>
+export default {
+    name: "Footer",
+        data() {
+        return {
+            currentYear: "",
+        };
+    },
+    mounted() {
+        this.currentYear = new Date().getFullYear();
+    },
+};
+</script>
