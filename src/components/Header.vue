71f79c6c09c82867b9452178686afcd7158b1e54
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2021-07-06 11:09:43 +0300

    move to component

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <header
+        class="relative flex flex-row justify-between shadow-md bg-capeHoney-default md:rounded-lg md:pl-2"
+    >
+        <g-link to="/">
+            <g-image src="~/assets/img/Safari_Juciaca_Logo-1935x583.png" width="200" />
+        </g-link>
+        <Nav />
+    </header>
+</template>
+
+<script>
+import Nav from "~/components/Nav";
+export default {
+    name: "Header",
+    components: {
+        Nav
+    },
+};
+</script>
