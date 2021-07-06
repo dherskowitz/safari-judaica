1bcdca634c5820d3ed51dab678ffce140f1eaecd
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2021-07-06 11:39:37 +0300

    merge components

---
+++ src/components/ShopMenu.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="w-64 bg-white rounded-lg shadow-lg">
+        <p class="pt-4 mx-2 mb-6 font-semibold border-b-2 border-gray-700">{{ title }}</p>
+
+        <g-link
+            class="block w-full px-4 py-2 category-item"
+            v-for="edge in menuItems"
+            :key="edge.node.id"
+            :to="edge.node.path"
+            exact
+        >{{ edge.node.name }}</g-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ShopMenu",
+    props:  {
+        title: String,
+        menuItems: Array
+    }
+};
+</script>
