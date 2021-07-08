daa651beffe10310ac20f4f2a01a15c5b273244d
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2021-07-08 09:33:25 +0300

    move code to components

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,22 +1,8 @@
 <template>
     <div class="container mx-auto">
-        <header
-            class="relative flex flex-row bg-capeHoney md:rounded-lg shadow-md justify-between md:pl-2"
-        >
-            <g-link to="/">
-                <g-image src="~/assets/img/Safari_Juciaca_Logo-1935x583.png" width="200" />
-            </g-link>
-            <Nav />
-        </header>
+        <Header/>
         <slot />
-        <footer class="mt-20 bg-outerSpace">
-            <div class="p-10">
-                <g-image src="~/assets/img/Safari_Juciaca_Logo-1024x864.png" width="200" />
-            </div>
-            <div
-                class="bg-outerSpace-lighter py-3 text-center text-white"
-            >Copyright {{ currentYear }} All Rights Reserved SafariJudaica.com</div>
-        </footer>
+        <Footer/>
     </div>
 </template>
 
@@ -29,18 +15,12 @@ query {
 </static-query>
 
 <script>
-import Nav from "~/components/Nav";
+import Header from "~/components/Header";
+import Footer from "~/components/Footer";
 export default {
     components: {
-        Nav,
-    },
-    data() {
-        return {
-            currentYear: "",
-        };
-    },
-    mounted() {
-        this.currentYear = new Date().getFullYear();
+        Header,
+        Footer
     },
 };
 </script>
