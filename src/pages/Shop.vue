2f3a246f594c2555d635c5afd4dabd3eea99af73
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2021-07-08 09:33:46 +0300

    use shop layout

--- src/pages/Shop.vue
+++ src/pages/Shop.vue
@@ -1,19 +1,16 @@
 <template>
-    <Layout>
-        <section class="min-h-screen md:flex md:items-start md:max-w-full">
-            <div class="mt-10 lg:mt-20 flex-none w-full md:max-w-xs">
-                <CategoriesMenu />
-            </div>
+    <Shop>
+        <section class="relative flex">
             <div class="flex-1 w-full">
                 <NewProducts />
                 <PopularProducts colCount="3" />
             </div>
         </section>
-    </Layout>
+    </Shop>
 </template>
 
 <script>
-import CategoriesMenu from "~/components/CategoriesMenu";
+import Shop from '~/layouts/Shop.vue'
 import PopularProducts from "~/components/PopularProducts";
 import NewProducts from "~/components/NewProducts";
 export default {
@@ -22,7 +19,7 @@ export default {
         meta: [{ name: "description", content: "Shop Safari Judaica" }],
     },
     components: {
-        CategoriesMenu,
+        Shop,
         PopularProducts,
         NewProducts,
     },
