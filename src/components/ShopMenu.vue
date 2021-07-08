6346d19dd979c4cf980d92226ebd1c8b93a5ef3c
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2021-07-08 09:32:45 +0300

    change active class

--- src/components/ShopMenu.vue
+++ src/components/ShopMenu.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="w-64 bg-white rounded-lg shadow-lg">
+    <div class="w-64 mb-4 bg-white rounded-lg shadow-lg">
         <p class="pt-4 mx-2 mb-6 font-semibold border-b-2 border-gray-700">{{ title }}</p>
 
         <g-link
-            class="block w-full px-4 py-2 category-item"
+            class="block w-full px-4 py-2 category-item shop-link"
             v-for="edge in menuItems"
             :key="edge.node.id"
             :to="edge.node.path"
@@ -21,3 +21,9 @@ export default {
     }
 };
 </script>
+
+<style scoped>
+.shop-link.active {
+    background-color: #bda27f;
+}
+</style>
