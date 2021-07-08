28df82e77e7f64c1c8a326ef9dc8180cc77580bf
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2021-07-08 10:08:40 +0300

    change width

--- src/components/ShopMenu.vue
+++ src/components/ShopMenu.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-64 mb-4 bg-white rounded-lg shadow-lg">
+    <div class="w-full mb-4 bg-white rounded-lg md:w-64 md:shadow-lg">
         <p class="pt-4 mx-2 mb-6 font-semibold border-b-2 border-gray-700">{{ title }}</p>
 
         <g-link
