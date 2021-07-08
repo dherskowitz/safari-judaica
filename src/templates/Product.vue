22c57d585e239e6d3af045c28c3ed62dfb7622be
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2021-07-08 09:34:46 +0300

    formatting, tailwind class sorting

--- src/templates/Product.vue
+++ src/templates/Product.vue
@@ -1,29 +1,29 @@
 <template>
     <Layout>
-        <div class="mt-10">
+        <!-- <div class="mt-10">
             <g-link to="/shop" class="mr-1 hover:underline">Shop</g-link>/
             <g-link
                 :to="categoryLink"
                 class="mr-1 hover:underline"
             >{{ $page.product.categoryName[0] }}</g-link>/
             <span class="text-capeHoney-alt">{{ $page.product.name }}</span>
-        </div>
-        <div class="mt-10 md:bg-white flex flex-col md:flex-row">
+        </div> -->
+        <div class="flex flex-col mt-10 md:bg-white md:flex-row">
             <div
-                class="bg-white rounded-lg md:rounded-none shadow-lg md:shadow-none w-full md:w-1/2 md:mr-10 flex flex-col justify-between border-capeHoney-alt md:border-r-4"
+                class="flex flex-col justify-between w-full bg-white rounded-lg shadow-lg md:rounded-none md:shadow-none md:w-1/2 md:mr-10 border-capeHoney-alt md:border-r-4"
             >
                 <div class="mainProductImage">
                     <g-image
                         :src="mainImage.url"
                         :alt="$page.product.name"
-                        class="max-h-full mx-auto w-full object-cover"
+                        class="object-cover w-full max-h-full mx-auto"
                     ></g-image>
                 </div>
                 <div v-if="$page.product.additionalImages.length > 0" class="flex flex-wrap">
                     <g-image
                         :src="$page.product.primaryImage[0].thumbnails.large.url"
                         :alt="$page.product.name"
-                        class="w-1/3 cursor-pointer border-capeHoney-alt border-t-4 border-r-4"
+                        class="w-1/3 border-t-4 border-r-4 cursor-pointer border-capeHoney-alt"
                         :class="{'active-image': mainImage.id == $page.product.primaryImage[0].id}"
                         @click="changeMainImage($page.product.primaryImage[0])"
                     ></g-image>
@@ -32,18 +32,18 @@
                         :src="img.thumbnails.large.url"
                         :alt="$page.product.name"
                         :key="img.id"
-                        class="w-1/3 cursor-pointer border-capeHoney-alt border-t-4 border-r-4"
+                        class="w-1/3 border-t-4 border-r-4 cursor-pointer border-capeHoney-alt"
                         :class="{'active-image': mainImage.id == img.id}"
                         @click="changeMainImage(img)"
                     ></g-image>
                 </div>
             </div>
             <div
-                class="bg-white rounded-lg md:rounded-none shadow-lg md:shadow-none w-full md:w-1/2 md:mr-10 text-center md:text-left mt-10 md:mt-0 pt-4 pb-4 leading-loose"
+                class="w-full pt-4 pb-4 mt-10 leading-loose text-center bg-white rounded-lg shadow-lg md:rounded-none md:shadow-none md:w-1/2 md:mr-10 md:text-left md:mt-0"
             >
-                <h1 class="text-3xl mb-2 border-b-4 border-capeHoney-alt">{{ $page.product.name }}</h1>
+                <h1 class="mb-2 text-3xl border-b-4 border-capeHoney-alt">{{ $page.product.name }}</h1>
                 <div
-                    class="flex flex-col md:flex-row mt-4 justify-center md:justify-start items-center"
+                    class="flex flex-col items-center justify-center mt-4 md:flex-row md:justify-start"
                     v-if="productReviews.length > 0"
                 >
                     <span>
@@ -58,8 +58,8 @@
                     <p class="text-sm md:ml-4">Based on {{ productReviews.length }} reviews.</p>
                 </div>
                 <p
-                    class="py-2 italic text-xl text-grey-600 font-semibold"
-                >${{ $page.product.priceDollars.toFixed(2) }}</p>
+                    class="py-2 text-xl italic font-semibold text-grey-600"
+                >{{ $page.product.priceDollars ? "$"+$page.product.priceDollars.toFixed(2) : "" }}</p>
                 <p>{{ $page.product.stockStatus }}</p>
                 <p>{{ $page.product.summary }}</p>
                 <p>
@@ -67,28 +67,28 @@
                     <span v-for="catName, i in $page.product.categoryName">
                         <span v-if="i > 0">/&nbsp;&nbsp;</span>
                         <a
-                            class="text-capeHoney-alt underline text-left"
+                            class="text-left underline text-capeHoney-alt"
                             :href="'/shop/'+catName.replace(' ', '-').toLowerCase()"
                         >{{ catName }}</a>&nbsp;&nbsp;
                     </span>
                 </p>
                 <p>
-                    <span class="text-lg font-bold text-gray-700">- Dimensions</span>&nbsp;&nbsp; 10"w x 20"h
+                    <span class="text-lg font-bold text-gray-700">- Dimensions</span>&nbsp;&nbsp; lorem ipsum
                 </p>
                 <p>
-                    <span class="text-lg font-bold text-gray-700">- Weight</span>&nbsp;&nbsp; 0.5 lbs
+                    <span class="text-lg font-bold text-gray-700">- Weight</span>&nbsp;&nbsp; lorem lbs
                 </p>
                 <p>
-                    <span class="text-lg font-bold text-gray-700">- Materials</span>&nbsp;&nbsp; Leather and Fur
+                    <span class="text-lg font-bold text-gray-700">- Materials</span>&nbsp;&nbsp; lorem ipsum
                 </p>
-                <div class="mt-5 flex flex-wrap justify-center lg:justify-between">
+                <div class="flex flex-wrap justify-center mt-5 lg:justify-between">
                     <span class="mt-2 lg:mt-0">
                         <p class="block mb-2">Like Us On:</p>
                         <a
                             href="#"
                             target="_blank"
                             rel="noopener noreferrer nofollow"
-                            class="mr-2 mb-2 border-gray-400 hover:bg-capeHoney-lighter cursor-pointer border-2 py-3 px-6"
+                            class="px-6 py-3 mb-2 mr-2 border-2 border-gray-400 cursor-pointer hover:bg-capeHoney-lighter"
                         >Facebook</a>
                     </span>
                     <span class="mt-2 lg:mt-0">
@@ -97,7 +97,7 @@
                             href="https://www.instagram.com/safari.rosy/"
                             target="_blank"
                             rel="noopener noreferrer nofollow"
-                            class="mr-2 mb-2 border-gray-400 hover:bg-capeHoney-lighter cursor-pointer border-2 py-3 px-6"
+                            class="px-6 py-3 mb-2 mr-2 border-2 border-gray-400 cursor-pointer hover:bg-capeHoney-lighter"
                         >Instagram</a>
                     </span>
                     <span class="mt-2 lg:mt-0">
@@ -107,38 +107,38 @@
                             data-action="share/whatsapp/share"
                             target="_blank"
                             rel="noopener noreferrer nofollow"
-                            class="hidden md:inline mr-2 mb-2 border-gray-400 hover:bg-capeHoney-lighter cursor-pointer border-2 py-3 px-6"
+                            class="hidden px-6 py-3 mb-2 mr-2 border-2 border-gray-400 cursor-pointer md:inline hover:bg-capeHoney-lighter"
                         >WhatsApp</a>
                         <a
                             :href="'whatsapp://send'+whatsappShare"
                             data-action="share/whatsapp/share"
                             target="_blank"
                             rel="noopener noreferrer nofollow"
-                            class="inline md:hidden mr-2 mb-2 border-gray-400 hover:bg-capeHoney-lighter cursor-pointer border-2 py-3 px-6"
+                            class="inline px-6 py-3 mb-2 mr-2 border-2 border-gray-400 cursor-pointer md:hidden hover:bg-capeHoney-lighter"
                         >WhatsApp</a>
                     </span>
                 </div>
 
                 <button
-                    class="bg-outerSpace p-2 px-4 mt-10 text-white rounded-lg shadow-lg md:w-full uppercase text-base md:text-lg"
+                    class="p-2 px-4 mt-10 text-base text-white uppercase rounded-lg shadow-lg bg-outerSpace-default md:w-full md:text-lg"
                     @click="toggleForm"
                 >Inquire about this product!</button>
             </div>
         </div>
         <div class="mt-10 md:mt-20">
-            <h2 class="text-3xl mb-2 border-b-4 border-capeHoney-alt">Product Description</h2>
-            <p class="bg-white rounded-lg shadow-lg mt-10 p-10">{{ $page.product.description }}</p>
+            <h2 class="mb-2 text-3xl border-b-4 border-capeHoney-alt">Product Description</h2>
+            <p class="p-10 mt-10 bg-white rounded-lg shadow-lg">{{ $page.product.description }}</p>
         </div>
         <div class="mt-10 md:mt-20" v-if="productReviews.length > 0">
-            <h2 class="text-3xl mb-2 border-b-4 border-capeHoney-alt">Product Reviews</h2>
-            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
+            <h2 class="mb-2 text-3xl border-b-4 border-capeHoney-alt">Product Reviews</h2>
+            <div class="grid grid-cols-1 gap-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
                 <span
-                    class="mb-2 w-full sm:max-w-xs lg:max-w-2xl"
+                    class="w-full mb-2 sm:max-w-xs lg:max-w-2xl"
                     v-for="rev in productReviews"
                     :key="rev.id"
                 >
                     <blockquote
-                        class="relative p-4 text-xl italic border-l-4 bg-white shadow-lg rounded-lg text-neutral-600 border-neutral-500 quote"
+                        class="relative p-4 text-xl italic bg-white border-l-4 rounded-lg shadow-lg text-neutral-600 border-neutral-500 quote"
                     >
                         <p class="mb-4">"{{ rev.comment }}"</p>
                         <Star
@@ -162,14 +162,16 @@
             </div>
         </div>
         <div class="mt-10 md:mt-20">
-            <h2 class="text-3xl mb-2 border-b-4 border-capeHoney-alt">Submit A Review</h2>
-            <div class="bg-white rounded-lg shadow-lg mt-10 p-10">
+            <h2 class="mb-2 text-3xl border-b-4 border-capeHoney-alt">Submit A Review</h2>
+            <div class="p-10 mt-10 bg-white rounded-lg shadow-lg">
                 <ProductReviewForm />
             </div>
         </div>
         <div class="mt-10 md:mt-20" v-if="relatedProducts.length > 0">
-            <h2 class="text-3xl mb-8 border-b-4 border-capeHoney-alt">Similar Products</h2>
-            <ProductCard v-for="edge in relatedProducts" :key="edge.node.id" :productData="edge" />
+            <h2 class="mb-8 text-3xl border-b-4 border-capeHoney-alt">Similar Products</h2>
+            <div class="flex space-x-4">
+                <ProductCard v-for="edge in relatedProducts" :key="edge.node.id" :productData="edge" />
+            </div>
         </div>
         <ProductInquiryForm
             v-bind:showForm="showForm"
@@ -184,14 +186,12 @@ query ($id: ID!, $Category__0: String!) {
     product: product(id: $id) {
         id
         name
-        sku
         path
         priceDollars
         category
         categoryName
         stockStatus
         newProduct
-        summary
         description
         averageRating
         primaryImage {
@@ -225,14 +225,13 @@ query ($id: ID!, $Category__0: String!) {
             remainAnonymous
         }
     }
-    relatedProducts: allProduct(filter: {category: {contains: [$Category__0]}}) {
+    relatedProducts: allProduct(filter: {category: {contains: [$Category__0]}}, limit: 4) {
         edges {
             node {
                 id
                 name
                 path
                 priceDollars
-                summary
                 stockStatus
                 primaryImage {
                     thumbnails {
@@ -277,22 +276,23 @@ export default {
         };
     },
     metaInfo() {
+        let vm = this;
         return {
-            title: this.$page.product.name,
+            title: vm.$page.product.name,
             meta: [
                 { name: "description", content: "Shop Safari Judaica" },
                 {
                     property: "og:title",
-                    content: this.$page.product.name,
+                    content: vm.$page.product.name,
                 },
                 {
                     property: "og:description",
-                    cotent: this.$page.product.summary,
+                    cotent: vm.$page.product.summary,
                 },
                 {
                     property: "og:image",
                     content:
-                        this.$page.product.primaryImage[0].thumbnails.large
+                        vm.$page.product.primaryImage[0].thumbnails.large
                             .url || "",
                 },
             ],
